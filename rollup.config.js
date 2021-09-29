4e435f068d02b08fbec1c5aa42248e25efa698b3
Ricardo Vela <aricav96@gmail.com>
2021-09-28 19:12:23 -0500

    Setup basic app skeleton

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import resolve from '@rollup/plugin-node-resolve';
+import livereload from 'rollup-plugin-livereload';
+import serve from 'rollup-plugin-serve';
+import path from 'path';
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: 'src/main.js',
+  output: {
+      file: 'dist/app.js',
+  },
+  plugins: [
+    resolve(),
+    terser(),
+    serve(),
+    livereload({
+      watch: [
+        path.resolve(__dirname, 'dist'),
+      ],
+      exts: [ 'html', 'js', 'css' ]
+    }),
+  ]
+};
