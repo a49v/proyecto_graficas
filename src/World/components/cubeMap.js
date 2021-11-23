f8b6be00b6fb9a31c1bd3e39de64ce5e4c83171a
Ricardo Vela <aricav96@gmail.com>
2021-11-22 22:57:23 -0600

    Change skybox to a better(?) one(??)

--- src/World/components/cubeMap.js
+++ src/World/components/cubeMap.js
@@ -1,15 +1,26 @@
 import { CubeTextureLoader, sRGBEncoding } from 'three';
 
 const reflectionCube = new CubeTextureLoader()
-      .setPath('assets/textures/cube/milky_way/')
+      .setPath('assets/textures/cube/space/')
       .load([
-        'dark-s_px.jpg',
-        'dark-s_nx.jpg',
-        'dark-s_py.jpg',
-        'dark-s_ny.jpg',
-        'dark-s_pz.jpg',
-        'dark-s_nz.jpg'
+        'space_rt.png',
+        'space_lf.png',
+        'space_up.png',
+        'space_dn.png',
+        'space_ft.png',
+        'space_bk.png'
       ]);
+
+// const reflectionCube = new CubeTextureLoader()
+//       .setPath('assets/textures/cube/milky_way/')
+//       .load([
+//         'dark-s_px.jpg',
+//         'dark-s_nx.jpg',
+//         'dark-s_py.jpg',
+//         'dark-s_ny.jpg',
+//         'dark-s_pz.jpg',
+//         'dark-s_nz.jpg'
+//       ]);
 
 reflectionCube.encoding = sRGBEncoding;
 
